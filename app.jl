0e688ac5dd1838b51744e040d319ba4a7fbc9cb0
IvanSelin <ivanselin93@gmail.com>
2023-10-03 01:38:40 +0300

    code cleanup

--- app.jl
+++ app.jl
@@ -7,8 +7,6 @@ using PlotlyBase
 using Statistics
 using DataFrames
 # using StippleUI
-# include(joinpath("lib","track.jl"))
-# include(joinpath("lib","weather.jl"))
 include("lib\\solar_loader.jl")
 @genietools
 
@@ -31,19 +29,6 @@ mkpath(joinpath(FILE_PATH, WEATHER_DIR))
 function mean(x)
     sum(x) / length(x)
 end
-
-# @handlers begin
-#     @in calculating = false
-#     @out is_calculating = false
-#     @out calculation_progress = 0.
-
-#     @onchange calculating begin
-#         is_calculating = true
-#         calculation_progress = 0.
-#         sleep(0.5)
-#         calculation_progress = 0.5
-#     end
-# end
 
 # add reactive code to make the UI interactive
 @app begin
@@ -74,24 +59,6 @@ end
         marker_color="fuchsia",
         marker_size=4
       )]
-    # @out track_trace = [scatter(
-    #     x=[1, 2, 3, 4],
-    #     y=[10, 15, 13, 17],
-    #     mode="lines+markers",
-    #     name="Trace 1"
-    # )]
-    # @out track_layout = PlotlyBase.Layout(
-    #     title="A Scatter Plot with Multiple Traces",
-    #     xaxis=attr(
-    #         title="X Axis Label",
-    #         showgrid=false
-    #     ),
-    #     yaxis=attr(
-    #         title="Y Axis Label"
-    #         # showgrid=true,
-    #         # range=[0, 20]
-    #     )
-    # )
 
     @out track_layout = PlotlyBase.Layout(
             dragmode="zoom",
@@ -140,69 +107,6 @@ end
         calculation_progress = 100
         is_calculating = false
     end
-   
-    # @onchange start_index begin
-    #     # index_out = "sample text "*string(start_index)
-
-    #     before_df = track_df[1:start_index, :]
-    #     after_df = track_df[start_index:end,:]
-
-    #     lats_after = after_df.latitude
-    #     lons_after = after_df.longitude
-
-    #     lats_before = before_df.latitude
-    #     lons_before = before_df.longitude
-
-    #     trace_after = scatter(
-    #         type="scattermapbox",
-    #         lon=lons_after,
-    #         lat=lats_after,
-    #         marker_color="red",
-    #         line_color="red",
-    #         marker_size=4,
-    #         mode="lines+markers",
-    #         name="prediction"
-    #     )
-    #     trace_before = scatter(
-    #         type="scattermapbox",
-    #         lon=lons_before,
-    #         lat=lats_before,
-    #         marker_color="green",
-    #         line_color="green",
-    #         marker_size=4,
-    #         mode="lines+markers",
-    #         name="passed"
-    #     )
-
-
-    #     # lats = [30., 30.]
-    #     # lons = [-100., -101.]
-    #     # track_trace = [scatter(
-    #     #     type="scattermapbox",
-    #     #     text=[ "1", "2" ],
-    #     #     lon=lons,
-    #     #     lat=lats,
-    #     #     marker_color="fuchsia",
-    #     #     marker_size=4
-    #     # )]
-
-    #     track_trace = [trace_before, trace_after]
-    #     track_layout = PlotlyBase.Layout(
-    #         dragmode="zoom",
-    #         mapbox_style="open-street-map",
-    #         mapbox_center_lat=mean(track_df.latitude),
-    #         mapbox_center_lon=mean(track_df.longitude),
-    #         # mapbox_zoom=2,
-    #         autosize=true,
-    #         height=300,
-    #         margin_l=0,
-    #         margin_r=0,
-    #         margin_t=0,
-    #         margin_b=0,
-    #         legend_title_side="top",
-    #         legend_orientation="h"
-    #     )
-    # end
 
     @onchange start_datetime begin
         
@@ -220,22 +124,6 @@ end
         # start_datetime = string(Dates.now())
         start_datetime = Dates.format(now(), "YYYY-mm-dd HH:MM:SS")
     end
-
-    # function get_track_plot_traces(start_index)
-    #     before_df = track_df[1:start_index, :]
-    #     after_df = track_df[start_index:max_index,:]
-
-    #     lats = after_df.latitude
-    #     lons = after_df.longitude
-    #     println()
-    #     trace_after = scatter(
-    #         type="scattermapbox",
-    #         lon=lons,
-    #         lat=lats,
-    #         marker_color="fuchsia",
-    #         marker_size=4
-    #     )
-    # end
 
     function get_map_traces(track_data, split_index, weather_df)
         before_df = track_data[1:split_index, :]
@@ -275,17 +163,6 @@ end
             z=weather_df.z,
             opacity=0.5
         )
-        
-        # lats = [30., 30.]
-        # lons = [-100., -101.]
-        # track_trace = [scatter(
-        #     type="scattermapbox",
-        #     text=[ "1", "2" ],
-        #     lon=lons,
-        #     lat=lats,
-        #     marker_color="fuchsia",
-        #     marker_size=4
-        # )]
 
         traces = [trace_before, trace_after, weather_trace]
         # track_traces = [trace_before, trace_after]
@@ -306,19 +183,6 @@ end
             legend_title_side="top",
             legend_orientation="h"
         )
-
-        # track_layout = PlotlyBase.Layout(
-        #     dragmode="zoom",
-        #     mapbox_style="open-street-map",
-        #     mapbox_center_lat=mean(track_df.latitude),
-        #     mapbox_center_lon=mean(track_df.longitude),
-        #     mapbox_zoom=3,
-        #     height=300,
-        #     margin_l=0,
-        #     margin_r=0,
-        #     margin_t=0,
-        #     margin_b=0
-        # )
 
         return traces, layout
     end
@@ -355,71 +219,6 @@ end
         start_index = min(start_index, max_track_index)
         track_traces, track_layout = get_map_traces(track_df, start_index, weather_density_df)
     end
-
-    # @onchange selected_track begin
-    #     # println(typeof(selected_track))
-    #     println(selected_track)
-    #     track_df, segments_df = get_track_and_segments(joinpath(FILE_PATH, TRACK_DIR, selected_track))
-    #     max_track_index = size(segments_df,1)
-    #     start_index = min(start_index, max_track_index)
-
-    #     before_df = track_df[1:start_index, :]
-    #     after_df = track_df[start_index:end,:]
-
-    #     lats_after = after_df.latitude
-    #     lons_after = after_df.longitude
-
-    #     lats_before = before_df.latitude
-    #     lons_before = before_df.longitude
-
-    #     trace_after = scatter(
-    #         type="scattermapbox",
-    #         lon=lons_after,
-    #         lat=lats_after,
-    #         marker_color="red",
-    #         line_color="red",
-    #         marker_size=4,
-    #         mode="lines+markers",
-    #         name="prediction"
-    #     )
-    #     trace_before = scatter(
-    #         type="scattermapbox",
-    #         lon=lons_before,
-    #         lat=lats_before,
-    #         marker_color="green",
-    #         line_color="green",
-    #         marker_size=4,
-    #         mode="lines+markers",
-    #         name="passed"
-    #     )
-
-
-    #     # lats = [30., 30.]
-    #     # lons = [-100., -101.]
-    #     # track_trace = [scatter(
-    #     #     type="scattermapbox",
-    #     #     text=[ "1", "2" ],
-    #     #     lon=lons,
-    #     #     lat=lats,
-    #     #     marker_color="fuchsia",
-    #     #     marker_size=4
-    #     # )]
-
-    #     track_trace = [trace_before, trace_after]
-    #     track_layout = PlotlyBase.Layout(
-    #         dragmode="zoom",
-    #         mapbox_style="open-street-map",
-    #         mapbox_center_lat=mean(track_df.latitude),
-    #         mapbox_center_lon=mean(track_df.longitude),
-    #         mapbox_zoom=3,
-    #         height=300,
-    #         margin_l=0,
-    #         margin_r=0,
-    #         margin_t=0,
-    #         margin_b=0
-    #     )
-        
-    # end
 
     route("/track", method = POST) do
         files = Genie.Requests.filespayload()
@@ -466,18 +265,5 @@ meta = Dict(
 layout = DEFAULT_LAYOUT(meta = meta)
 @page("/", "app.jl.html", layout)
 @page("/code", ui)
-
-
-# route("/weather", method = POST) do
-#     files = Genie.Requests.filespayload()
-#     for f in files
-#         write(joinpath(FILE_PATH, WEATHER_DIR, f[2].name), f[2].data)
-#     end
-#     weather_files = readdir(joinpath(FILE_PATH, WEATHER_DIR))
-#     if length(files) == 0
-#         @info "No file uploaded"
-#     end
-#     return "Upload finished"
-# end
 
 end
